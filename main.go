569ca6738cf953728ddf0ea3e7b725ceab101ae2
codychen <suigetuyamo@gmail.com>
2019-10-10 02:41:16 +0800

    fix ini error

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"os/exec"
+	"path/filepath"
 	"strings"
 	"time"
 
@@ -13,7 +15,7 @@ import (
 )
 
 func log(msg string) {
-	logFile, _ := os.OpenFile("./home/logs/detechHealthy.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	logFile, _ := os.OpenFile("/home/logs/detechHealthy.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	info := fmt.Sprintf("[%s] %s", time.Now().Format("2006-01-02 15:04:05.99999"), msg)
 	logFile.WriteString(info)
 }
@@ -56,7 +58,11 @@ type Config struct {
 }
 
 func loadConfig() (config Config, err error) {
-	cfg, err := ini.Load("./config.ini") //load config
+	file, _ := exec.LookPath(os.Args[0])
+	path, _ := filepath.Abs(file)
+	index := strings.LastIndex(path, string(os.PathSeparator))
+
+	cfg, err := ini.Load(path[:index] + "/config.ini") //load config
 	if err != nil {
 		return config, err
 	}
